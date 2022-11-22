86761d29033c5f635faf3f111eecf370b9762922
mauveyone <101948515+mauveyone@users.noreply.github.com>
2022-11-22 20:16:27 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>TicTacToe</title>
+<script src="https://cdn.tailwindcss.com"></script>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-600 py-6 sm:py-12">
+  	<div class="relative bg-white p-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+    	<div class="mx-auto max-w-md">
+			<h2 class="text-3xl font-bold tracking-tight text-gray-900 text-4xl pb-10">TicTacToe</h2>
+			<div class="grid grid-rows-3 grid-flow-col gap-4">
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3"></div>
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3"></div>
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3"></div>
+
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3"></div>
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3"></div>
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3"></div>
+
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3"></div>
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3"></div>
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3"></div>
+				
+				
+				<!--
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
+					  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</div>
+				
+				<div class="box-border h-20 w-20 rounded-lg bg-gray-100 shadow-inner hover:bg-gray-200 grid place-content-center p-3">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
+					  <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+					</svg>
+				-->
+				</div>
+  	  		</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
